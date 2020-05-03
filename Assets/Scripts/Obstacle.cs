881de2828ae00022d8c5a183ff8a18947a8a0798
Saga <barros.sebag@gmail.com>
2020-05-03 12:51:38 -0300

    los obstáculos quedan firmes cuando tocan el suelo

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Floor"))
+        {
+            rb.isKinematic = true;
+        }
+
+    }
+}
