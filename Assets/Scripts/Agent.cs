485520058469fcff9f54141626e06416b8aa434b
Saga <barros.sebag@gmail.com>
2020-05-04 18:10:35 -0300

    varias cosas
    Puertas de destino creadas.
    Agentes van hacia las puertas automaticamente.
    Haciendo click en el suelo se instancian figuras

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -6,48 +6,30 @@ using UnityEngine.AI;
 public class Agent : MonoBehaviour
 {
     NavMeshAgent agent;
-    Vector3 hitPoint;
+    GameObject[] destinations;
+    Transform destination;
 
     // Start is called before the first frame update
     void Start()
     {
+        destinations = GameObject.FindGameObjectsWithTag("Destination");
+
+        destination = destinations[Random.Range(0, destinations.Length)].transform;
         agent = GetComponent<NavMeshAgent>();
+
+        setTarget(destination.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
-            CheckClick();
-        }
-    }
-
-    void CheckClick()
-    {
-        //bool clickOnFloor;
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
         
-
-        if (Physics.Raycast(ray,out hit))
-        {
-            hitPoint = hit.point;
-            if (hit.collider.gameObject.CompareTag("Floor"))
-            {
-                setTarget(hit.point);
-            }
-        }
     }
+
+    
 
     void setTarget(Vector3 target)
     {
        agent.destination = target;
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawSphere(hitPoint, 2);
-    }
+    }    
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector3 hitPoint;
+    public GameObject[] pieces;
+    int current;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CheckClick();
+        }
+
+        void CheckClick()
+        {
+            //bool clickOnFloor;
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                hitPoint = hit.point;
+                if (hit.collider.gameObject.CompareTag("Floor") && current < pieces.Length)
+                {
+                    DropPiece(hit.point);
+                }
+            }
+        }
+    }
+
+    void DropPiece(Vector3 location)
+    {
+        Vector3 spawnPos = new Vector3(location.x, location.y + 8, location.z);
+        Instantiate(pieces[current], spawnPos, Quaternion.identity);
+        current++;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(hitPoint, 2);
+    }
+}
